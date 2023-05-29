23a218723d6e36b04bbea5deac2a9380d6acfa07
Ejikeme Dumaka <dumakaejikeme@gmail.com>
2023-05-29 22:58:21 +0100

    Completed the Testimonial, investors and traction sections

---
+++ src/app/home-page/traction/traction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-traction',
+  templateUrl: './traction.component.html',
+  styleUrls: ['./traction.component.scss']
+})
+export class TractionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
