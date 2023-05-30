fd6287a0e73f674643fbc5ea9b2ba303a2da594d
Ejikeme Dumaka <dumakaejikeme@gmail.com>
2023-05-30 01:25:13 +0100

    testimonial fix

--- src/app/home-page/testimonials/testimonials.component.ts
+++ src/app/home-page/testimonials/testimonials.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.scss']
+  styleUrls: ['./testimonials.component.scss'],
 })
 export class TestimonialsComponent implements OnInit {
-
-  constructor() { }
+  isTestimonyActive: boolean = true;
+  constructor() {}
 
   ngOnInit(): void {
+    const testimonials = document.querySelectorAll(
+      '.testimonial-section .info-box'
+    ) as any;
+    for (const testimonial of testimonials) {
+      testimonial.addEventListener('mousemove', (e: any) => {
+        if (!testimonial.classList.contains('first-testimonial')) {
+          this.isTestimonyActive = false;
+        } else {
+          this.isTestimonyActive = true;
+        }
+      });
+      testimonial.addEventListener('mouseout', (e: any) => {
+        this.isTestimonyActive = true;
+      });
+    }
   }
-
 }
